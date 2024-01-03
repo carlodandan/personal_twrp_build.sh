42a4a24591c74ecbe24ef626bedcf919f2851b6a
Carlo Dandan <carlodandan.personal@proton.me>
2024-01-03 07:33:13 +0000

    twrp_build_script: added more conditional statements
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- twrp.sh
+++ twrp.sh
@@ -43,15 +43,19 @@ else
 fi
 
 # Environment
-export DEBIAN_FRONTEND=noninteractive
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y install rsync gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev lib32ncurses5-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc bc ccache lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 lzop pngcrush schedtool squashfs-tools imagemagick libbz2-dev lzma ncftp qemu-user-static libstdc++-10-dev libtinfo5 libgflags-dev python3 python-is-python3
-echo "Done setting up build environment."
+echo "Update Build Environment? [yes/no]"
+read ask_build_env
+if [ "${ask_build_env}" = "yes" ]; then
+  export DEBIAN_FRONTEND=noninteractive
+  sudo apt -y update && \
+  sudo apt -y upgrade && \
+  sudo apt -y install rsync gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev lib32ncurses5-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc bc ccache lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 lzop pngcrush schedtool squashfs-tools imagemagick libbz2-dev lzma ncftp qemu-user-static libstdc++-10-dev libtinfo5 libgflags-dev python3 python-is-python3
+  echo "Done setting up build environment."
+fi
 
 # Git-Repo Tool
 mkdir -p ~/bin
-if [ ! -d ~/bin/repo ]; then
+if [ ! -f ~/bin/repo ]; then
   curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
   chmod a+x ~/bin/repo
   sudo ln -sf ~/bin/repo /usr/bin/repo
@@ -89,7 +93,3 @@ source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch ${DEVICE_MAKEFILE}-eng && mka ${BUILD_TARGET}image
 echo "Your TWRP is now ready. Check in $OUT folder"
-
-
-
-
