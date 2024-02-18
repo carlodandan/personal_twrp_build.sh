3d3e489a7402fedf025af4530d5bb97959254bf8
Carlo Dandan <carlodandan.personal@proton.me>
2024-02-18 15:51:32 +0000

    twrp_build_script: Switched to transfer.sh for cloud upload. * Added output redirection, to create a text file named download.txt, that contains download link.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- upload.sh
+++ upload.sh
@@ -8,6 +8,6 @@ cd ${BUILD_DIRECTORY}
 source build/envsetup.sh
 lunch ${DEVICE_MAKEFILE}-eng
 
-curl bashupload.com -T $OUT/${BUILD_TARGET}.img
+curl --upload-file $OUT/${BUILD_TARGET}.img https://transfer.sh/${BUILD_TARGET}.img > ${PROJECT_DIR}/download.txt
 
 cd ${PROJECT_DIR}
